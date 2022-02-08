a3771a063956a008ee0643166a403bf057f2da8d
dokkano <>
2022-02-08 10:15:58 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<head>
+  <title><?php echo file_get_contents('school-name.txt'); ?> フォームサイト</title>
+</head>
+<body style="margin:0px;" onCopy="return false;">
+  <div style="position:fixed; width:100%;background-color:#C0D0FF; box-shadow: 3px 3px 3px 3px rgba(0,0,0,0.3); higth:30%">
+    <center>
+    <p><font size="6"><?php echo file_get_contents('school-name.txt'); ?> フォームサイト</font></p>
+    </center>
+  </div>
+  <h4>このサイトは転載禁止です。</h4>
+  <h4>^^</h4>
+</body>
+<body>
+  <center>
+    <h1><?php echo file_get_contents('school-name.txt'); ?><br>フォームサイトへようこそ！</h1>
+    <p>Welcoem to <?php echo file_get_contents('english-school-name.txt'); ?> form site!</p>
+  </center>
+  <fieldset>
+    <legend><h2 align="center">Q. このサイトってどんなものなの？</h2></legend>
+    <p>このサイトは<?php echo file_get_contents('school-name.txt'); ?>で自由に使えるアンケートサイトです。</p>
+    <p>イベントやクラスのアンケートなどに使えます。</p>
+  </fieldset>
+</body>
